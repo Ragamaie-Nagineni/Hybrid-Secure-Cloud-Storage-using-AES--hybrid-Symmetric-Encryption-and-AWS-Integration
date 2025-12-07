d452aab476aa41a7db782fd94169fb8e9d9e16df
Ragamaie <ragamaie.n@gmail.com>
2025-12-07 13:56:21 +0530

    Remove unused encryption code from encryptor.py
    Removed commented-out code for encryption functions.

--- backend/encryptor.py
+++ backend/encryptor.py
@@ -1,15 +1,1 @@
-# from cryptography.fernet import Fernet
 
-# # Use a fixed key (same key every time)
-# # Replace this string with the one you generated above
-# key = b"rnDFLecaKEOy81CuW33XE5JmdWZzhRwfRhr6oFq2ngg="
-
-# fernet = Fernet(key)
-
-# def encrypt_text(text):
-#     """Encrypt plain text using Fernet symmetric encryption"""
-#     return fernet.encrypt(text.encode()).decode()
-
-# def decrypt_text(cipher):
-#     """Decrypt cipher text back to plain text"""
-#     return fernet.decrypt(cipher.encode()).decode()
